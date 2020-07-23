a0e439a1ca7430c71f8cc26eb5ca800d9f2a0748
KhashayarZikow <68709290+KhashayarZikow@users.noreply.github.com>
2020-07-23 23:59:17 +0430

    EmployeeData added

---
+++ Employee/DepartmentData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee
+{
+    public class DepartmentData: IHost
+    {
+    }
+}
---
+++ Employee/IHost.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee
+{
+    public interface IHost
+    {
+    }
+}
